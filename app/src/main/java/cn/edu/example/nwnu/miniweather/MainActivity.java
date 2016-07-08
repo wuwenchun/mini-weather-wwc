8215ca969f1c1477c7714a3e62789be72a641dbd
wuwenchun <496194722@qq.com>
2016-07-08 18:12:03 +0800

    first commit

---
+++ app/src/main/java/cn/edu/example/nwnu/miniweather/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.example.nwnu.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zzp on 2016/7/8.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
